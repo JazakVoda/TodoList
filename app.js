d390bb3da500c05d5e9f3d366b08722569b4af05
JazakVoda <blazin.steva@gmail.com>
2018-04-24 01:48:34 +0200

    Third Commit

--- app.js
+++ app.js
@@ -17,22 +17,24 @@
 	// Push new task in array of tasks
 	function tasksPush(){
 		const text = document.getElementById('text').value;
-		if(text == ''){
-			text.value = alert('Write something!');
+		const date = document.getElementById('date').value;
+		if(text == '' || date == ''){
+			text.value = alert('Add Task and Date!');
 		} else{
 			tasks.push({
 				id: Date.now(),
 				date: getDate(),
 				text: text,
-				isCorect: false
+				isCorect: false,
+				todoDate: date
 			});
+			sort();
 			generateList();
 			reset();
 		}
 	}
-
 	function generateList(){
-		document.getElementById("myTable").innerHTML = '<th>Date/Time</th><th>Description</th><th>Action</th>';
+		document.getElementById("myTable").innerHTML = '<th>Date/Time</th><th>Description</th><th>Date</th><th>Action</th>';
 
 		tasks.forEach(function(task, index){
 			const table = document.getElementById("myTable");
@@ -42,6 +44,24 @@
 		    const cell1 = row.insertCell(0);
 		    const cell2 = row.insertCell(1);
 		    const cell3 = row.insertCell(2);
+		    const cell4 = row.insertCell(3);
+
+		    //Calculate dates
+			const dateNow = Date.UTC(new Date(getDate()).getFullYear(), new Date(getDate()).getMonth(), new Date(getDate()).getDate());
+			const dateTodo = Date.UTC(new Date(task.todoDate).getFullYear(), new Date(task.todoDate).getMonth(), new Date(task.todoDate).getDate());
+
+			const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+			const day = (dateTodo - dateNow) / _MS_PER_DAY;
+			//console.log(dateNow);
+			console.log(dateTodo);
+
+			if(day === 0){
+				table.rows[index + 1].classList.add('today');
+			} 
+			if(day < 0) {
+				table.rows[index + 1].classList.add('past');
+			}
+
 
 		    //Delete Task
 		    const btn = document.createElement("BUTTON");
@@ -54,6 +74,7 @@
 			//Blue color, task done!    
 			const checked = document.createElement('BUTTON');
 			checked.addEventListener('click', function(){
+				table.rows[index + 1].classList.remove('today');
 				table.rows[index + 1].classList.add('selected');
 				task.isCorect = true;
 				checked.style.display = 'none';
@@ -66,9 +87,9 @@
 			const e = document.createTextNode('\u2611');
 			checked.appendChild(e);
 			checked.style.color = 'blue';
-			
-			//After deleteTask generate selected rows
+
 			if(task.isCorect === true){
+				table.rows[index + 1].classList.remove('today');
 				table.rows[index + 1].classList.add('selected');
 				checked.setAttribute("style", "display: none;");
 			}            
@@ -76,16 +97,26 @@
 			//Push items from tasks in new row
 		    cell1.innerHTML = task.date;
 		    cell2.innerHTML = task.text;
-		    cell3.appendChild(btn);
-		    cell3.appendChild(checked); 
+		    cell3.innerHTML = task.todoDate;
+		    cell4.appendChild(btn);
+		    cell4.appendChild(checked);
 
 		    saveTasks();
 		});   
 	}
 
+	// Date sort
+	function sort(){
+		tasks.sort(function(a, b) {
+	    	// convert date object into number to resolve issue in typescript
+	    	return +new Date(a.todoDate) - +new Date(b.todoDate);
+		});
+	}
+
 	// Reset input field
 	function reset(){
 		document.getElementById('text').value = '';
+		document.getElementById('date').value = '';
 
 	}
 
@@ -119,7 +150,7 @@
 		if(mm<10){
 		    mm='0'+mm;
 		} 
-		return today = dd+'/'+mm+'/'+yyyy+ ' ' + hours + ':' + minutes;
+		return today = yyyy+'/'+mm+'/'+dd+ ' ' + hours + ':' + minutes;
 	}
 
 
