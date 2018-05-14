5656ef8e57f06d2b3b364f2f1f495dd2a4a76f8c
JazakVoda <blazin.steva@gmail.com>
2018-05-14 21:42:25 +0200

    New COMMIT

---
+++ js/list.js
@@ -0,0 +1,26 @@
+
+  document.getElementById('list').addEventListener('click', getExchangeList);
+
+  function getExchangeList(){
+    fetch('http://data.fixer.io/api/latest?access_key=5d4f06f5acc5e91c3b5ba070bfcdf825&format=1')
+      .then((res) => res.json())
+      .then((data) => {
+        var output = '';
+          output += `
+            <div class="list1">
+              <h2>${data.base}</h2>
+              <p class="date">${data.date}<p>
+              <div class="flags">
+              <p><img src="flags/rs.png" width=40px;> RSD = <span class="bold">${data.rates.RSD}</span></p>
+              <p><img src="flags/us.png" width=40px;> USD = <span class="bold">${data.rates.USD}</span></p>
+              <p><img src="flags/ch.png" width=40px;> CHF = <span class="bold">${data.rates.CHF}</span></p>
+              <p><img src="flags/gb.png" width=40px;> GBP = <span class="bold">${data.rates.GBP}</span></p>
+              <p><img src="flags/ca.png" width=40px;> CAD = <span class="bold">${data.rates.CAD}</span></p>
+              <p><img src="flags/hr.png" width=40px;> HRK = <span class="bold">${data.rates.HRK}</span></p>
+              </div>
+            
+            </div>
+          `;
+        document.getElementById('show-list').innerHTML = output;
+    })
+  }
--- js/weather.js
+++ js/weather.js
@@ -1,33 +1,4 @@
- 
 
-
-  // var request = new XMLHttpRequest();
-
-  // // Open a new connection, using the GET request on the URL endpoint
-  // request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?lat=45.267136&lon=19.833549&APPID=c470d9117220df245654821524d1b0c2', true);
-
-  // request.onload = function () {
-  //   // Begin accessing JSON data here
-  //   var data = JSON.parse(this.response);
-
-  //   if (request.status >= 200 && request.status < 400) {
-  //       const city = data.name;
-  //       const tem = data.main.temp;
-  //       const wind = data.weather.id;
-  //       const humidity = '';  
-
-  //       document.getElementById('city').innerHTML = city;
-  //       document.getElementById('weather').innerHTML = tem;
-  //       document.getElementById('wind').innerHTML = wind;
-  //   } else {
-  //       const errorMessage = document.createElement('marquee');
-  //       errorMessage.textContent = `Gah, it's not working!`;
-  //       app.appendChild(errorMessage);
-  //   }
-  // }
-
-  // // Send request
-  // request.send();
   document.getElementById('weather').addEventListener('click', getWeather);
 
   function getWeather(){
